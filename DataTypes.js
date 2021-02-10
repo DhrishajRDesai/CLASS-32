7995710424f2ce5315e358d59df0db76771c389f
DhrishajRDesai <78604785+DhrishajRDesai@users.noreply.github.com>
2021-02-10 18:39:27 +0530

    Add files via upload

---
+++ DataTypes.js
@@ -0,0 +1,37 @@
+var ob1= "Welcome";
+console.log(typeof ob1)
+var ob2= 44
+console.log(typeof ob2);
+var ob3= "1144";
+console.log(typeof ob3);
+var ob4 = {
+    "Name": "Dhrishaj Desai",
+    "Age": "14"
+}
+console.log(typeof ob4);
+var ob5 ;
+console.log(typeof ob5);
+var ob6 = null;
+console.log(typeof ob6);
+var a = [10,20,30,40,50];
+console.log(typeof a);
+console.log(a);
+a.push("Dhrishaj");
+a.push("Ruturaj");
+console.log(a);
+console.log(a.length);
+a.pop()
+console.log(a);
+for(var i=0;i<a.length;i++){
+    console.log(a[i]);
+}
+a=[10,20]
+b=[11,20]
+c=[44,26]
+d=[]
+d.push(a)
+d.push(b)
+d.push(c)
+console.log(d);
+console.log(d[2][1]);
+console.log(d[1][0]);
